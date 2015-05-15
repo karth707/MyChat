307581e6dee7af83efb31dd02b769916287b9fb2
nikhilesh20 <nikhileshp92@gmail.com>
2015-05-15 02:00:52 -0400

    Test

--- server/server.py
+++ server/server.py
@@ -1,13 +1,31 @@
-from flask import Flask
+from flask import Flask, session, redirect, url_for, escape, request
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "Start!!"
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return redirect(url_for('login'))
 
-@app.route("/hello")
-def hello():
-    return 'Hello World'
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+# set the secret key.  keep this really secret:
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+app.run()
